072b898535ead0e311b8b2b83b09ecea8b8e172d
lela_jovanovic <ljovanovc5919rn@raf.rs>
2020-11-04 00:46:25 +0100

    AboutDialog- postavka

--- src/dsw/rudok/app/gui/swing/view/AboutDialog.java
+++ src/dsw/rudok/app/gui/swing/view/AboutDialog.java
@@ -1,6 +1,24 @@
 package dsw.rudok.app.gui.swing.view;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class AboutDialog  extends JDialog {
+
+    private ImageIcon img1, img2;
+    private JLabel lelaOpis, andrijanaOpis;
+    private JLabel lelaSl, andrijanaSl ;
+
+    public  AboutDialog(){
+
+        setTitle("About info");
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = kit.getScreenSize();
+        int screenHeight = screenSize.height;
+        int screenWidth = screenSize.width;
+        setSize(screenWidth / 3, screenHeight / 2);
+        setLocationRelativeTo(null);
+
+
+    }
 }

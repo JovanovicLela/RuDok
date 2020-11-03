56c17b355b95afd9fd98685efb31e03574c9f49d
lela_jovanovic <ljovanovc5919rn@raf.rs>
2020-11-04 00:03:43 +0100

    tree/controller/CellEditor update

--- src/dsw/rudok/app/gui/swing/tree/controller/RuTreeCellEditor.java
+++ src/dsw/rudok/app/gui/swing/tree/controller/RuTreeCellEditor.java
@@ -1,4 +1,56 @@
 package dsw.rudok.app.gui.swing.tree.controller;
 
-public class RuTreeCellEditor {
+import dsw.rudok.app.gui.swing.tree.model.RuTreeItem;
+import dsw.rudok.app.repository.Workspace;
+
+import javax.swing.*;
+import javax.swing.tree.DefaultTreeCellEditor;
+import javax.swing.tree.DefaultTreeCellRenderer;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.util.EventObject;
+
+public class RuTreeCellEditor extends DefaultTreeCellEditor implements ActionListener {
+
+    private  Object clickedOn = null;
+    private JTextField edit = null;
+
+    public RuTreeCellEditor(JTree tree, DefaultTreeCellRenderer renderer) {
+        super(tree, renderer);
+    }
+
+    public Component getTreeCellEditorComponent(JTree arg0, Object arg1, boolean arg2, boolean arg3, boolean arg4, int arg5){
+        clickedOn = arg1;
+        edit = new JTextField(arg1.toString());
+        edit.addActionListener(this);
+        return  edit;
+    }
+
+    @Override
+    public boolean isCellEditable(EventObject event) {
+        if(event instanceof MouseEvent)
+            if(((MouseEvent)event).getClickCount()==3){
+                return  true;
+            }
+        return  false;
+    }
+
+    public  void actionPerformed(ActionEvent e){
+
+        if(!(clickedOn instanceof RuTreeItem))
+            return;
+        RuTreeItem clicked = (RuTreeItem) clickedOn;
+
+        if(clicked.getNodeModel() instanceof Workspace){
+            clicked.setName(e.getActionCommand());
+            ((Workspace) clicked.getNodeModel()).setName(e.getActionCommand());
+        }
+        else {
+            ((Workspace) clicked.getNodeModel()).setName(e.getActionCommand());
+        }
+
+
+    }
 }

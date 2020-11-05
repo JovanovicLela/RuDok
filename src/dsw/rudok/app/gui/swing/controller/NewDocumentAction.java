7d6a6dc4e4a9361091bdae35300501a1c2b2f1b5
lela_jovanovic <ljovanovc5919rn@raf.rs>
2020-11-05 23:51:34 +0100

    ProjectPanel je i sam observer (osvežava se svaki put kad god se nesto doda u stablo)

--- src/dsw/rudok/app/gui/swing/controller/NewDocumentAction.java
+++ src/dsw/rudok/app/gui/swing/controller/NewDocumentAction.java
@@ -1,17 +1,45 @@
 package dsw.rudok.app.gui.swing.controller;
 
 import dsw.rudok.app.gui.swing.observer.Observer;
+import dsw.rudok.app.gui.swing.view.MainFrame;
+import dsw.rudok.app.repository.Document;
+import dsw.rudok.app.repository.Project;
+import dsw.rudok.app.repository.node.RuNode;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 
 public class NewDocumentAction extends  AbstractRudokAction implements Observer {
+
+    public  NewDocumentAction(){
+        putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_D, ActionEvent.CTRL_MASK));
+        putValue(SMALL_ICON, loadIcon("images/newDocument1.png"));
+        putValue(NAME, "New Document");
+        putValue(SHORT_DESCRIPTION, "New Document");
+
+        setEnabled(false);
+        MainFrame.getInstance().getSubject().attach(this);
+    }
     @Override
     public void update() {
-
+        if(MainFrame.getInstance().getTree().getSelectedRuNode() instanceof Project){
+            setEnabled(true);
+            setEnabled(false);
+        }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
 
+        RuNode selectedRuNode = MainFrame.getInstance().getTree().getSelectedRuNode();
+
+        if((selectedRuNode == null) || !(selectedRuNode instanceof Project)) // ako nije selektovan projekat, nema dodavanja novog Doc
+            return;
+
+        Project selectedProject = (Project) selectedRuNode;
+        Document document = new Document("Document " + (selectedProject.getChildren().size() +1),selectedProject);
+        MainFrame.getInstance().getTree().addDocument(selectedProject, document);
+        MainFrame.getInstance().getSubject().notifyAllObservers();
     }
 }
--- src/dsw/rudok/app/gui/swing/view/ProjectPanel.java
+++ src/dsw/rudok/app/gui/swing/view/ProjectPanel.java
@@ -1,17 +1,32 @@
 package dsw.rudok.app.gui.swing.view;
 
-import javax.swing.*;
+import dsw.rudok.app.gui.swing.observer.Observer;
+import dsw.rudok.app.repository.Project;
 
-public class ProjectPanel extends JPanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class ProjectPanel extends JPanel implements Observer {
 
     private  JLabel projectNameLabel = new JLabel();
+    private Project openedProject = null;
+    private  JTabbedPane tabbedPane = new JTabbedPane();
 
     public ProjectPanel(){
-        add(projectNameLabel);
+        setLayout(new BorderLayout());
+        add(projectNameLabel, BorderLayout.NORTH);
+        add(tabbedPane);
+
+        MainFrame.getInstance().getSubject().attach(this);
     }
 
     public void setProjectName(String projectName) {
-
         projectNameLabel.setText(projectName);
+    }
+
+
+    @Override
+    public void update() {
+
     }
 }

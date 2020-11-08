04e77641c4105d60ed41ced78e65b86b6c2e99d5
lela_jovanovic <ljovanovc5919rn@raf.rs>
2020-11-08 10:29:03 +0100

    mouseListener

--- src/dsw/rudok/app/gui/swing/tree/view/RuTreeView.java
+++ src/dsw/rudok/app/gui/swing/tree/view/RuTreeView.java
@@ -1,6 +1,7 @@
 package dsw.rudok.app.gui.swing.tree.view;
 
 import dsw.rudok.app.gui.swing.tree.controller.RuTreeCellEditor;
+import dsw.rudok.app.gui.swing.tree.controller.RuTreeMouseListener;
 import dsw.rudok.app.gui.swing.tree.controller.RuTreeSelectionListener;
 
 import javax.swing.*;
@@ -11,7 +12,9 @@ public class RuTreeView extends JTree {
     public RuTreeView(DefaultTreeModel defaultTreeModel){
         setModel(defaultTreeModel);
         RuTreeCellRenderer ruTreeCellRenderer = new RuTreeCellRenderer();
+
         addTreeSelectionListener(new RuTreeSelectionListener());
+        addMouseListener(new RuTreeMouseListener());
         setCellEditor(new RuTreeCellEditor(this, ruTreeCellRenderer));
         setCellRenderer(ruTreeCellRenderer);
         setEditable(true);

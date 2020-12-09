95ca96ce3d36a21d92a424e392aba135b9c1dd3a
lela_jovanovic <ljovanovc5919rn@raf.rs>
2020-12-09 16:18:17 +0100

    kompletni DevicePainter i DiDev

--- src/dsw/rudok/app/gui/swing/view/painters/DevicePainter.java
+++ src/dsw/rudok/app/gui/swing/view/painters/DevicePainter.java
@@ -2,9 +2,11 @@ package dsw.rudok.app.gui.swing.view.painters;
 
 import dsw.rudok.app.repository.elements.DiagramDevice;
 import dsw.rudok.app.repository.elements.DiagramElement;
+import dsw.rudok.app.repository.elements.InputOutputElement;
 
 import java.awt.*;
 import java.awt.geom.AffineTransform;
+import java.util.Iterator;
 
 public class DevicePainter extends  ElementPainter{
 
@@ -29,14 +31,31 @@ public class DevicePainter extends  ElementPainter{
         if(element instanceof DiagramDevice){
             g.setPaint(Color.BLACK);
             DiagramDevice device = (DiagramDevice)element;
+
+            // ako crtamo neki elem, potrbni su nam i jnegovi ulazi i izlazi
+            Iterator it = device.getInputIterator();
+            while (it.hasNext()){
+                InputOutputElement d = (InputOutputElement) it.next();
+                d.getElementPainter().paint(g, d);
+            }
+
+            Iterator it2 = device.getOutputIterator();
+            while (it2.hasNext()){
+                InputOutputElement d2 = (InputOutputElement) it2.next();
+                d2.getElementPainter().paint(g, d2);
+            }
+
             g.drawString(device.getName(), (int)device.getPosition().getX()+10, (int)device.getPosition().getY()+10);
-
         }
-        g.setTransform(old);
 
+        // ovde crtamo elem i linkove
 
+        g.setPaint(element.getStrokeColor());
+        g.setStroke(element.getStroke());
+        g.draw(getShape());
 
-
+        g.setPaint(element.getPaint());
+        g.fill(getShape());
     }
 
     public boolean isElementAt( Point pos){
--- src/dsw/rudok/app/repository/elements/DiagramDevice.java
+++ src/dsw/rudok/app/repository/elements/DiagramDevice.java
@@ -2,6 +2,8 @@ package dsw.rudok.app.repository.elements;
 
 import java.awt.*;
 import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public abstract class DiagramDevice extends  DiagramElement{
 
@@ -11,18 +13,76 @@ public abstract class DiagramDevice extends  DiagramElement{
     protected  int inputNo;
     protected  int outputNo;
 
+    protected ArrayList<InputOutputElement> inputs = new ArrayList<InputOutputElement>();
+    protected ArrayList<InputOutputElement> outputs = new ArrayList<InputOutputElement>();
+
+
     public DiagramDevice(Point2D position, Dimension size, Stroke stroke, Paint paint, Color strokeColor, int inputNo, int outputNo) {
         super(stroke, paint, strokeColor);
         this.size = size;
 
-        position.setLocation(position.getX()-size.getWidth()/2, position.getY()-size.getHeight()/2);
+        position.setLocation(position.getX()-size.getWidth()/2, position.getY()-size.getHeight()/2); // translacija tacaka da bi se elem kreirao u centru
+
         this.position = position;
         this.strokeColor = strokeColor;
         this.inputNo = inputNo;
         this.outputNo = outputNo;
 
+        //ulaz
+        for(int i = 0; i < inputNo; i++){
+
+            Point2D ioPos = new Point2D.Double(position.getX(), position.getY() + (getSize().getHeight()/ (getInputNo() + 1) * (i+1)));
+            addInput((InputOutputElement) InputOutputElement.createDefault(ioPos, stroke, paint, this, i+1, InputOutputElement.INPUT));
+
+        }
+
+        //izlaz
+        for (int i = 0;i < outputNo ;i++){
+
+            Point2D ioPos = new Point2D.Double(position.getX()+size.width, position.getY() + (getSize().getHeight()/ (getOutputNo() + 1)) * (i+1)) ;
+            addOutput((InputOutputElement) InputOutputElement.createDefault(ioPos,stroke,paint,this,i+1,InputOutputElement.OUTPUT));
+
+        }
 
     }
+    // metode potrebne za rad sa kolekcijom ulaza
+    public  void addInput(InputOutputElement p){
+        inputs.add(p);
+    }
+    public void removeInput(InputOutputElement p){
+        inputs.remove(p);
+    }
+    public InputOutputElement getInputAt(int i){
+        return inputs.get(i);
+    }
+    public  int getInputCount(){
+        return  inputs.size();
+    }
+    public Iterator getInputIterator(){
+        return inputs.iterator();
+    }
+    // *******************************************************
+
+
+    //  metode za rad kolekcijom izlaza
+
+    public void addOutput(InputOutputElement p){
+        outputs.add(p);
+    }
+    public void removeOutput(InputOutputElement p){
+        outputs.remove(p);
+    }
+    public InputOutputElement getOutputAt(int i){
+        return outputs.get(i);
+    }
+    public int getIOutputCount(){
+        return outputs.size();
+    }
+    public Iterator getOutputIterator(){
+        return outputs.iterator();
+    }
+    // ******************************************************
+
 
     public Dimension getSize() {
         return size;
@@ -38,5 +98,37 @@ public abstract class DiagramDevice extends  DiagramElement{
 
     public void setPosition(Point2D position) {
         this.position = position;
+    }
+
+    public int getInputNo() {
+        return inputNo;
+    }
+
+    public void setInputNo(int inputNo) {
+        this.inputNo = inputNo;
+    }
+
+    public int getOutputNo() {
+        return outputNo;
+    }
+
+    public void setOutputNo(int outputNo) {
+        this.outputNo = outputNo;
+    }
+
+    public ArrayList<InputOutputElement> getInputs() {
+        return inputs;
+    }
+
+    public void setInputs(ArrayList<InputOutputElement> inputs) {
+        this.inputs = inputs;
+    }
+
+    public ArrayList<InputOutputElement> getOutputs() {
+        return outputs;
+    }
+
+    public void setOutputs(ArrayList<InputOutputElement> outputs) {
+        this.outputs = outputs;
     }
 }

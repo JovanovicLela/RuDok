053f6af989d81ddcb0dae5bc267a199958ada7e0
lela_jovanovic <ljovanovc5919rn@raf.rs>
2020-12-22 14:50:44 +0100

    selekcija

--- src/dsw/rudok/app/gui/swing/view/PageView.java
+++ src/dsw/rudok/app/gui/swing/view/PageView.java
@@ -53,6 +53,7 @@ public class PageView extends JInternalFrame{
     public Slot getSelectedSlot() {
         return selectedSlot;
     }
+
     public int getNumberOfSlotsPerType(SlotFactory.SlotType slotType){
         if(slotType == null){
             AppCore.getInstance().getError().onError(ErrorType.NULL_SLOT);
@@ -65,7 +66,7 @@ public class PageView extends JInternalFrame{
 
         public Framework() {
             FrameworkAdapter frameworkAdapter = new FrameworkAdapter();
-            addMouseListener(new FrameworkAdapter());
+            addMouseListener(frameworkAdapter);
             addMouseMotionListener(frameworkAdapter);
         }
 
@@ -74,11 +75,11 @@ public class PageView extends JInternalFrame{
 
             Graphics2D g2 = (Graphics2D) g; // ALt + enter(quick fix)
 
-           //providnost
-             g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.8f));
-             g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            //providnost
+            g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.8f));
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-            for(int i = 0; i < page.getChildren().size(); i++){
+            for (int i = 0; i < page.getChildren().size(); i++) {
                 Slot slot = (Slot) page.getChildren().get(i);
                 SlotView.drawShape(slot, g2);
             }
@@ -88,30 +89,30 @@ public class PageView extends JInternalFrame{
             @Override
             public void mousePressed(MouseEvent e) {
                 Slot slot = null;
-                if (MainFrame.getInstance().getStateManager().getCurrentState() instanceof CircleSate){
+                if (MainFrame.getInstance().getStateManager().getCurrentState() instanceof CircleSate) {
                     slot = SlotFactory.createSlot("Slot" + (page.getChildren().size() + 1),
                             page, e.getX(), e.getY(), 40, 40, SlotFactory.SlotType.CIRCLE);
-                MainFrame.getInstance().getTree().addSlot(page, slot);
-            }else if(MainFrame.getInstance().getStateManager().getCurrentState() instanceof RectangleState){
-                    slot = SlotFactory.createSlot("Slot" + (page.getChildren().size() + 1),
-                            page, e.getX(), e.getY(),40,20, SlotFactory.SlotType.RECTANGLE);
                     MainFrame.getInstance().getTree().addSlot(page, slot);
-                }else if(MainFrame.getInstance().getStateManager().getCurrentState() instanceof TriangleState){
+                } else if (MainFrame.getInstance().getStateManager().getCurrentState() instanceof RectangleState) {
+                    slot = SlotFactory.createSlot("Slot" + (page.getChildren().size() + 1),
+                            page, e.getX(), e.getY(), 40, 20, SlotFactory.SlotType.RECTANGLE);
+                    MainFrame.getInstance().getTree().addSlot(page, slot);
+                } else if (MainFrame.getInstance().getStateManager().getCurrentState() instanceof TriangleState) {
                     slot = SlotFactory.createSlot("Slot" + (page.getChildren().size() + 1),
                             page, e.getX(), e.getY(), 40, 40, SlotFactory.SlotType.TRIANGLE);
                 }
 
-                 PageView.this.repaint();
+                PageView.this.repaint();
             }
 
             @Override
             public void mouseDragged(MouseEvent e) {
-                if(selectedSlot == null){
+                if (selectedSlot == null) {
                     return;
                 }
                 int x = e.getX();
-                int y  =e.getY();
-                if(SlotView.getDownerRightSelectionRectangle(selectedSlot).contains(x,y)) {
+                int y = e.getY();
+                if (SlotView.getDownerRightSelectionRectangle(selectedSlot).contains(x, y)) {
 
                     Rectangle2D selRectangle = SlotView.getDownerRightSelectionRectangle(selectedSlot);
                     Rectangle2D newSelRectangle = new Rectangle2D.Double(x - 2.5, y - 2.5, 5, 5);
@@ -119,19 +120,50 @@ public class PageView extends JInternalFrame{
                     double deltaX = newSelRectangle.getX() - selRectangle.getX();
                     double deltaY = newSelRectangle.getY() - selRectangle.getY();
 
-                    if(selectedSlot.getWidth() + deltaX > 0){
-                        double x0 = selectedSlot.getX() - (selectedSlot.getWidth()/2);
+                    if (selectedSlot.getWidth() + deltaX > 0) {
+                        double x0 = selectedSlot.getX() - (selectedSlot.getWidth() / 2);
                         selectedSlot.setWidth(selectedSlot.getWidth() + deltaX);
-                        double newX = x0 + selectedSlot.getWidth()/2;
+                        double newX = x0 + selectedSlot.getWidth() / 2;
                         selectedSlot.setX(newX);
                     }
 
+                    if(selectedSlot.getHeight() + deltaY > 0) {
+                        double y0 = selectedSlot.getY() - (selectedSlot.getHeight()/2);
+                        selectedSlot.setHeight(selectedSlot.getHeight() + deltaY);
+                        double newY = y0 + selectedSlot.getHeight()/2;
+                        selectedSlot.setY(newY);
+                    }
                 }
 
+                if (SlotView.getUpperLeftSelectionRectangle(selectedSlot).contains(x, y)) {
+
+                }
+                if (SlotView.getDownerRightSelectionRectangle(selectedSlot).contains(x, y)) {
+
+                }
+                if (SlotView.getDownerLeftSelectionRectangle(selectedSlot).contains(x, y)) {
+
+                }
+                PageView.this.repaint();
+            }
+
+            private Slot selectedSlot(int x, int y) {
+                for (int i = 0; i < page.getChildren().size(); i++) {
+                    Slot slot = (Slot) page.getChildren().get(i);
+                    Rectangle2D.Double rectangle = new Rectangle2D.Double((slot.getX() - slot.getWidth() / 2) - 10, (slot.getY() - slot.getHeight() / 2) - 10, slot.getWidth() + 20, slot.getHeight() + 20);
+                    if (rectangle.contains(x, y)) {
+                        return slot;
+                    }
+                }
+                return null;
+            }
+
+            private void deselectAllSlots() {
+                for (int i = 0; i < page.getChildren().size(); i++) {
+                    Slot slot = (Slot) page.getChildren().get(i);
+                    slot.setSelected(false);
+                }
             }
         }
-
-
     }
-
-}
+    }
